7df40d3a546d1433767c9920d7203f48a1d525be
Tremos <49289611+Tremos@users.noreply.github.com>
2024-01-10 11:11:05 +0600

    fix url

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://www.shop.kz/offers/smartfony"
+BASE_URL = "https://shop.kz/astana/offers/smartfony/"
 HEADERS = {'User-Agent': 'My User Agent 1.0'}
 JSON_FILE_PATH = "smartphones.json"
